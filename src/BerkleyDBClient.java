2d45392dcf3543908ac15c7e32bd43157d90ded3
Nelson Stoik <nstoik1@cs.ualberta.ca>
2015-04-04 15:42:44 -0600

    Change of structure

---
+++ src/BerkleyDBClient.java
@@ -0,0 +1,18 @@
+public class BerkleyDBClient {
+
+	void createAndPopulate() {
+	}
+
+	void retriveRecordsByKey() {
+	}
+
+	void retriveRecordsByData() {
+	}
+
+	void retriveRecordsByRange() {
+	}
+
+	void destoryDatabase() {
+	}
+
+}
---
+++ src/DatabaseConnection.java
@@ -0,0 +1,100 @@
+import java.util.Random;
+import com.sleepycat.db.*;
+
+/**
+ * @author nstoik1
+ *
+ */
+public class DatabaseConnection {
+	
+    // to specify the file name for the table
+    private static final String SAMPLE_TABLE = "/tmp/nstoik1_db/sample_table";
+    private static final int NO_RECORDS = 100;
+	
+	public static void createAndPopulate()  {
+	
+	try {
+
+	    //Create the database object.
+	  
+	    DatabaseConfig dbConfig = new DatabaseConfig();
+	    dbConfig.setType(DatabaseType.BTREE);
+	    dbConfig.setAllowCreate(true);
+	    dbConfig.setSortedDuplicates(true);
+	    
+	    Database my_table = new Database(SAMPLE_TABLE, null, dbConfig);
+	    System.out.println(SAMPLE_TABLE + " has been created");
+
+	    /* populate the new database with NO_RECORDS records */
+	    populateTable(my_table,NO_RECORDS);
+	    System.out.println(NO_RECORDS + " records inserted into" + SAMPLE_TABLE);
+
+	    /* cloase the database and the db enviornment */
+	    my_table.close();
+
+	    /* to remove the table */
+	   // my_table.remove(SAMPLE_TABLE,null,null);
+       
+        }
+        catch (Exception e1) {
+            System.err.println("Test failed: " + e1.toString());
+        }
+    }
+
+	/*
+	 * To pouplate the given table with nrecs records
+	 */
+	static void populateTable(Database my_table, int nrecs) {
+		int range;
+		DatabaseEntry kdbt, ddbt;
+		String s;
+
+		/*
+		 * generate a random string with the length between 64 and 127,
+		 * inclusive.
+		 * 
+		 * Seed the random number once and once only.
+		 */
+		Random random = new Random(1000000);
+
+		try {
+			for (int i = 0; i < nrecs; i++) {
+
+				/* to generate a key string */
+				range = 64 + random.nextInt(64);
+				s = "";
+				for (int j = 0; j < range; j++)
+					s += (new Character((char) (97 + random.nextInt(26))))
+							.toString();
+
+				/* to create a DBT for key */
+				kdbt = new DatabaseEntry(s.getBytes());
+				kdbt.setSize(s.length());
+
+				// to print out the key/data pair
+				System.out.println(s);
+
+				/* to generate a data string */
+				range = 64 + random.nextInt(64);
+				s = "";
+				for (int j = 0; j < range; j++)
+					s += (new Character((char) (97 + random.nextInt(26))))
+							.toString();
+				// to print out the key/data pair
+				System.out.println(s);
+				System.out.println("");
+
+				/* to create a DBT for data */
+				ddbt = new DatabaseEntry(s.getBytes());
+				ddbt.setSize(s.length());
+
+				/* to insert the key/data pair into the database */
+				my_table.putNoOverwrite(null, kdbt, ddbt);
+			}
+		} catch (DatabaseException dbe) {
+			System.err.println("Populate the table: " + dbe.toString());
+			System.exit(1);
+		}
+	}
+
+}
