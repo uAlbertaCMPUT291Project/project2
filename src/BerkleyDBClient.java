73efad0514e13b9f91f76ea3bd05a16e5ef519c0
Nelson Stoik <nstoik1@cs.ualberta.ca>
2015-04-04 18:26:29 -0600

    finished create and destroy of database. If database is not desroyed, it can be reloaded

--- src/BerkleyDBClient.java
+++ src/BerkleyDBClient.java
@@ -1,41 +1,60 @@
+import java.io.*;
 import java.util.Random;
 import com.sleepycat.db.*;
 
 public class BerkleyDBClient {
 
-	// to specify the file name for the table
-	private static final String SAMPLE_TABLE = "/tmp/nstoik1_db/sample_table";
+	//variable for if the database has been loaded or not
+	private static boolean DatabaseLoaded = false;
+	//database type: btree or hash or indexfile
+	private static String DatabaseTypeFromUser = null;
+	//databse location
+	private static final String DB_TABLE = "/tmp/nstoik1_db/db_table";
+	//number of records in the database
 	private static final int NO_RECORDS = 100;
+	//Database object
+	private static Database my_table;
+	//Cursor object
+	private static Cursor myCursor = null;
 
 	/*
 	 * Author: Nelson
 	 */
 	static void createAndPopulate() {
+		
+		if (DatabaseLoaded) {
+			System.out.println("A database has already been created at the given location");
+			System.out.println("Please destroy the database before creating a new one");
+			return;
+		}
+		
 		try {
 
 			// Create the database object.
-
 			DatabaseConfig dbConfig = new DatabaseConfig();
-			dbConfig.setType(DatabaseType.BTREE);
+			
+			if (DatabaseTypeFromUser.compareToIgnoreCase("btree") == 0) {
+				dbConfig.setType(DatabaseType.BTREE);
+			} else if (DatabaseTypeFromUser.compareToIgnoreCase("hash") == 0) {
+				dbConfig.setType(DatabaseType.HASH);
+			} else if (DatabaseTypeFromUser.compareToIgnoreCase("indexfile") == 0) {
+				//TODO: set up config file for indexfile
+				System.out.println("Still need to create the indexfile table");
+			}
 			dbConfig.setAllowCreate(true);
-			//dbConfig.setSortedDuplicates(true);
 
-			Database my_table = new Database(SAMPLE_TABLE, null, dbConfig);
-			System.out.println(SAMPLE_TABLE + " has been created");
+			my_table = new Database(DB_TABLE, null, dbConfig);
+			System.out.println(DB_TABLE + " has been created");
 
 			/* populate the new database with NO_RECORDS records */
-			populateTable(my_table, NO_RECORDS);
-			System.out.println(NO_RECORDS + " records inserted into"
-					+ SAMPLE_TABLE);
-
-			/* cloase the database and the db enviornment */
-			my_table.close();
-
-			/* to remove the table */
-			my_table.remove(SAMPLE_TABLE,null,null);
+			populateTable(NO_RECORDS);
+			System.out.println(NO_RECORDS + " records inserted into" + DB_TABLE);
+			
+			DatabaseLoaded = true;
 
 		} catch (Exception e1) {
-			System.err.println("Test failed: " + e1.toString());
+			System.err.println("createAndPopulate failed: " + e1.toString());
+			DatabaseLoaded = false;
 		}
 	}
 
@@ -59,16 +78,114 @@ public class BerkleyDBClient {
 
 	/*
 	 * Author: Nelson
+	 * Closes the database and then removes it from disk as well
 	 */
 	static void destoryDatabase() {
+
+		if(!DatabaseLoaded) {
+			System.out.println("No database to destroy");
+			return;
+		}
+		try {
+			/* Close the database and the db environment */
+			closeDB();
+
+			/* to remove the table */
+			my_table.remove(DB_TABLE, null, null);
+			
+			System.out.println(DB_TABLE + " closed and destroyed succesfully.");
+			DatabaseLoaded = false;
+			return;
+
+		} catch (Exception e1) {
+			System.err.println("destroyDatabase failed: " + e1.toString());
+		}
 	}
+	
+	
+	/* 
+	 * returns the path of the database location
+	 */
+	public static String getTableLocation() {
+		return DB_TABLE;
+	}
+	
+	/*
+	 * sets the private variable DatabaseTypeFromUser to the user input at
+	 * system startup
+	 */
+	public static void setDatabaseType(String newDatabaseType) {
+		
+		DatabaseTypeFromUser = newDatabaseType;
+		System.out.println("Databse type is: " + DatabaseTypeFromUser);
+	}
+	
+	/*
+	 * closes the database connection.
+	 * Called when the system is exiting or destroying the database
+	 * If the database is not destroyed, the database can be reopened later
+	 */
+	public static void closeDB() {
+		if(!DatabaseLoaded){
+			return;
+		}
+		
+		try {
+			my_table.close();
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+			System.out.println("Error closing Database!");
+		}
+	}
+	/*
+	 * if the database is not destroyed during the last session, it can
+	 * be reopened and used again
+	 */
+	public static boolean tryOpenExsistingDatabase() {
+		
+		try {
+			my_table = new Database(DB_TABLE, null, null);
+			DatabaseLoaded = true;
+			return true;
+			
+		} catch (FileNotFoundException e) {
+			DatabaseLoaded = false;
+			return false;
+		} catch (DatabaseException e) {
+			System.out.println("Database Exception");
+			DatabaseLoaded = false;
+			return false;
+		}
+		
+	}
+	
 
 	// -------------------------HELPER FUNCTIONS----------------------//
+	
+	private static void writeToFile(String key, String value) {
+		try
+		{
+		    String filename= "answers.txt";
 
+		    FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+		    fw.write(key + "\n");
+		    fw.write(value + "\n");
+		    fw.write("\n");
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+		}
+	}
+
+
+	
 	/*
 	 * To pouplate the given table with nrecs records
 	 */
-	static void populateTable(Database my_table, int nrecs) {
+	private static void populateTable(int nrecs) {
+
 		int range;
 		DatabaseEntry kdbt, ddbt;
 		String s;
@@ -96,7 +213,7 @@ public class BerkleyDBClient {
 				kdbt.setSize(s.length());
 
 				// to print out the key/data pair
-				 System.out.println("KEY: " + s);
+				//System.out.println("KEY: " + s);
 
 				/* to generate a data string */
 				range = 64 + random.nextInt(64);
@@ -105,8 +222,8 @@ public class BerkleyDBClient {
 					s += (new Character((char) (97 + random.nextInt(26))))
 							.toString();
 				// to print out the key/data pair
-				System.out.println("DATA: " + s);
-				System.out.println("");
+				//System.out.println("DATA: " + s);
+				//System.out.println("");
 
 				/* to create a DBT for data */
 				ddbt = new DatabaseEntry(s.getBytes());
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,6 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 //Author: Nelson
@@ -7,40 +9,65 @@ public class MainMenu {
 	private static int quit_code = 6;
 	private static int smallest_value = 1;
 	private static int largest_value = 6;
-	
+
 	//
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		int menu_choice = -1;
-		
-		//input validation
+
+		// input validation
 		if (args.length == 0) {
-			System.out.println("No input arguments, please enter a valid input argument.");
+			System.out
+					.println("No input arguments, please enter a valid input argument.");
 			return;
 		}
-		
-		if ((args[0].equals("btree")) || (args[0].equals("hash")) || (args[0].equals("indexfile"))) {
-			System.out.println("Good input");
+
+		if ((args[0].equals("btree")) || (args[0].equals("hash"))
+				|| (args[0].equals("indexfile"))) {
+			// System.out.println("Good input");
 		} else {
-			System.out.println("Incorrect input arguments. Arguments must be: 'btree' or 'hash' or 'indexfile'");
+			System.out.println("Incorrect input. Must be: 'btree' or 'hash' or 'indexfile'");
 			return;
 		}
-		
-		
-		System.out.println("Databse type is: " + args[0]);
-		
+
+		BerkleyDBClient.setDatabaseType(args[0]);
+		if(BerkleyDBClient.tryOpenExsistingDatabase()) {
+			System.out.print("An exsisting database has been loaded from: ");
+			System.out.println(BerkleyDBClient.getTableLocation());
+			System.out.println("To use a new database, destroy this one first (Option 5)");
+		}
+		else {
+			System.out.print("No database could be loaded from: ");
+			System.out.println(BerkleyDBClient.getTableLocation());
+			System.out.println("Please create a new database first (Option 1)");
+		}
+
 		while (menu_choice != quit_code) {
 			menu_choice = get_menu_input();
 			if (menu_choice != quit_code) {
-				//TODO: Launch required program
-				System.out.println("You have chosen option #" + menu_choice);
-				BerkleyDBClient.createAndPopulate();
+				switch (menu_choice) {
+				case 1:
+					BerkleyDBClient.createAndPopulate();
+					break;
+				case 2:
+					BerkleyDBClient.retriveRecordsByKey();
+					break;
+				case 3:
+					BerkleyDBClient.retriveRecordsByData();
+					break;
+				case 4:
+					BerkleyDBClient.retriveRecordsByRange();
+					break;
+				case 5:
+					BerkleyDBClient.destoryDatabase();
+				}
+
 			}
 
-			//delay until the user press enter to display the menu again
+			// delay until the user press enter to display the menu again
 			if (menu_choice != quit_code) {
 				System.out.println("Press ENTER to go back to the main menu.");
 				try {
@@ -51,9 +78,17 @@ public class MainMenu {
 			}
 
 		}
-		System.out.println("Exiting. Bye!");
+
+		BerkleyDBClient.closeDB();
+		//clear the answer file upon exiting of the program
+		try {
+			new PrintWriter("answer.txt").close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Closing database and exiting. Bye!");
 	}
-	
+
 	public static int get_menu_input() {
 
 		Scanner user_input = new Scanner(System.in);
@@ -65,7 +100,8 @@ public class MainMenu {
 			System.out.println("1: Create and Populate a database");
 			System.out.println("2: Retrieve records with a given key");
 			System.out.println("3: Retrieve records with a given data");
-			System.out.println("4: Retrieve records with a given range of key values");
+			System.out
+					.println("4: Retrieve records with a given range of key values");
 			System.out.println("5: Destroy the database");
 			System.out.println("6: Quit");
 
